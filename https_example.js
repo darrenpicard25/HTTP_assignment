b33c792de9aecbd81bc275f1f879668de44c90c3
Darren Picard <darrenpicard25@gmail.com>
2019-02-10 03:06:13 +0000

    added first bit of code

--- https_example.js
+++ https_example.js
@@ -1,1 +1,17 @@
-let http = require('http');
+let https = require('http');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+};
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
